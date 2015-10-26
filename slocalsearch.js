9262929f1ba153cf90b7eb7533e528eb52045113
jpdev2575 <Jeep60327>
2015-10-26 17:45:05 -0400

    add student Parser working

--- slocalsearch.js
+++ slocalsearch.js
@@ -1,11 +1,14 @@
-var fs = require('fs');
+var fs = require('fs'),
+	objectiveParser = require('objectiveParser'),
+	studentParser = require('studentParser');
 
-var objective = fs.readFileSync('objective.txt', {encoding: 'utf8'});
+var objective = objectiveParser(fs.readFileSync('objective.txt', {encoding: 'utf8'}));
 
-var classFile = fs.readFileSync('class.txt', {encoding: 'utf8'});
+var students = studentParser(fs.readFileSync('student.txt', {encoding: 'utf8'}));
 
-var studentFile = fs.readFileSync('student.txt', {encoding: 'utf8'});
+//var classFile = fs.readFileSync('class.txt', {encoding: 'utf8'});
 
-
+//var studentFile = fs.readFileSync('student.txt', {encoding: 'utf8'});
 
 console.log(objective);
+console.log(students);
