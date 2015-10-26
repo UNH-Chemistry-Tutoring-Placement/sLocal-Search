5a9cadd6398c9085f390227a01bd2373138568c6
Joel T Barna <jtf57@agate.cs.unh.edu>
2015-10-26 16:54:09 -0400

    Created rough objective parser

--- slocalsearch.js
+++ slocalsearch.js
@@ -1,11 +1,10 @@
-var fs = require('fs');
+var fs = require('fs'),
+	objectiveParser = require('objectiveParser');
 
-var objective = fs.readFileSync('objective.txt', {encoding: 'utf8'});
+var objective = objectiveParser(fs.readFileSync('objective.txt', {encoding: 'utf8'}));
 
-var classFile = fs.readFileSync('class.txt', {encoding: 'utf8'});
+//var classFile = fs.readFileSync('class.txt', {encoding: 'utf8'});
 
-var studentFile = fs.readFileSync('student.txt', {encoding: 'utf8'});
-
-
+//var studentFile = fs.readFileSync('student.txt', {encoding: 'utf8'});
 
 console.log(objective);
