39d747316190ff95d685bf18040c18befa80f115
jpdev2575 <Jeep60327>
2015-10-28 22:25:23 -0400

    add grade stuff

--- slocalsearch.js
+++ slocalsearch.js
@@ -2,9 +2,11 @@ var fs = require('fs'),
 	objectiveParser = require('objectiveParser'),
 	classParser = require('classParser'),
 	studentParser = require('studentParser'),
-	args = require('Args');
-	ranAssign = require('randomAssignment');
-
+	args = require('Args'),
+	ranAssign = require('randomAssignment'),
+    grader = require('Grade'),
+	graderWith = require('GradeWith'),
+	graderWithout = require('GradeWithout');
 
 var data = {};
 
@@ -15,5 +17,18 @@ data.class = classParser(fs.readFileSync('Class', {encoding: 'utf8'}));
 data.student = studentParser(fs.readFileSync('student.txt', {encoding: 'utf8'}));
 
 var times = ranAssign(data);
+var studentTest = []
+studentTest.push('daseg@wildcats.unh.edu')
+studentTest.push('robare@wildcats.unh.edu')
+studentTest.push('gemck@wildcats.unh.edu')
+studentTest.push('cahol@wildcats.unh.edu')
 
-console.log(times);
+console.log("orig: " + studentTest)
+var grade = grader(data,'Fri 14:00-15:00',studentTest);
+var gradeWith = graderWith(data,'Fri 14:00-15:00',studentTest,'amsal@wildcats.unh.edu');
+var gradeWithout = graderWithout(data,'Fri 14:00-15:00',studentTest,'daseg@wildcats.unh.edu');
+
+
+console.log("grade: " + grade);
+console.log("gradeWith: " + gradeWith);
+console.log("gradeWithout: " + gradeWithout);
